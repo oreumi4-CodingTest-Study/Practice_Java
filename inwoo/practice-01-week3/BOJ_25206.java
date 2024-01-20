de4884c851c37fb1188101e0b7ffba1467327c45
inwoo <jiw413@naver.com>
2024-01-21 01:19:41 +0900

    [BOJ] 통계학(2108) / 중하

--- inwoo/practice-01-week3/BOJ_25206.java
+++ inwoo/practice-01-week3/BOJ_25206.java
@@ -1,40 +1,40 @@
 import java.util.Scanner;
 
-public class BOJ_25206 {
-    public static void main (String[] args) {
+public class BOJ_25206 {    // 너의 평점은
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int subjectNum = 20;
         double creditSum = 0;
         double sum = 0;
-        for (int i=0; i<20; i++){
+        for (int i = 0; i < 20; i++) {
             String subject = sc.next();
             double credit = sc.nextDouble();
             String grade = sc.next();
 
-            if (grade.equals("A+")){
+            if (grade.equals("A+")) {
                 sum += credit * 4.5;
-            }else if (grade.equals("A0")){
+            } else if (grade.equals("A0")) {
                 sum += credit * 4;
-            }else if (grade.equals("B+")){
+            } else if (grade.equals("B+")) {
                 sum += credit * 3.5;
-            }else if (grade.equals("B0")){
+            } else if (grade.equals("B0")) {
                 sum += credit * 3;
-            }else if (grade.equals("C+")){
+            } else if (grade.equals("C+")) {
                 sum += credit * 2.5;
-            }else if (grade.equals("C0")){
+            } else if (grade.equals("C0")) {
                 sum += credit * 2;
-            }else if (grade.equals("D+")){
+            } else if (grade.equals("D+")) {
                 sum += credit * 1.5;
-            }else if (grade.equals("D0")){
+            } else if (grade.equals("D0")) {
                 sum += credit * 1;
-            }else if (grade.equals("F")){
+            } else if (grade.equals("F")) {
                 sum += credit * 0;
-            }else if (grade.equals("P")){
+            } else if (grade.equals("P")) {
                 continue;
             }
             creditSum += credit;
         }
-        System.out.printf("%.6f", sum/creditSum);
+        System.out.printf("%.6f", sum / creditSum);
     }
 }
