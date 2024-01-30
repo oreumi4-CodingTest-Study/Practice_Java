0d1f3936195928120e6ad35b254faaae3f9ffc14
inwoo <jiw413@naver.com>
2024-01-30 17:40:04 +0900

    [BOJ]  재귀의 귀재(25501) / 브론즈2 / O

---
+++ inwoo/M02/W3/BOJ_25501.java
@@ -0,0 +1,29 @@
+package M02.W3;
+
+import java.io.*;
+
+public class BOJ_25501 {
+    static int count = 0;
+    public static int recursion(String s, int l, int r){
+        count++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            bw.write(isPalindrome(br.readLine())+" "+count+"\n");
+            count = 0;
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
