c6e10268f26574c79ea16dc3591d84f385157a64
Jae Won Choi <58384329+bbabbungtting@users.noreply.github.com>
2024-01-30 17:42:29 +0900

    [BOJ] 카드2 / 실버4 / 20분
    Queue 관련 문제

--- jaewon/자료구조/카드2.java
+++ jaewon/자료구조/카드2.java
@@ -1,19 +1,21 @@
-//백준 2164번 '카드2' / 실버 4 / 20분
+//백준 2164번 '카드2' / 실버 4 / 20분 / Queue문제
 
 import java.util.*;
 
 public class Main {
     public static void main(String[] args)  {
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
-
+        
+        //큐 생성
         Queue<Integer> queue = new LinkedList<>();
 
+        //add : 큐에 원소 삽입
         for(int i=0;i<N;i++){
             queue.add(i+1);
         }
-
+        
+        //poll : 맨 위의 원소 삭제
         while(queue.size()!=1){
             queue.poll();
             queue.add(queue.poll());
