fc8898caf7d7e74d6fe76b0dbf3f6bb53eab3a27
inwoo <jiw413@naver.com>
2024-01-30 16:34:36 +0900

    [BOJ]  팩토리얼 2(27433) / 브론즈5 / O

---
+++ inwoo/M02/W3/BOJ_27433.java
@@ -0,0 +1,24 @@
+package M02.W3;
+
+import java.io.*;
+
+public class BOJ_27433 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        bw.write(Long.toString(factorial(N))+"\n");
+
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static long factorial(int N) {
+        if (N == 0) {
+            return 1l;
+        }
+        return N * (long)factorial(N-1);
+    }
+}
