4ecec267cf566172065732303c579f741c3dc67a
dhktjr0204 <dhktjr0204442@gmail.com>
2024-02-08 19:39:54 +0900

    [BOJ] 11722 가장 긴 감소하는 부분 수열/실버2

---
+++ jiwon/백준/num11722.java
@@ -0,0 +1,36 @@
+package silver;
+
+import java.io.*;
+import java.util.*;
+
+public class num11722 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 0);
+        dp[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j] && dp[i] <= dp[j]) {
+                    dp[i] = dp[j];
+                }
+            }
+            dp[i]++;
+        }
+        int max = 0;
+        for (int cnt : dp) {
+            max=Math.max(cnt,max);
+        }
+        System.out.println(max);
+    }
+}
